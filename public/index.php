cf941ebeb5e67b086053f024a426987868c2f528
Weiyixmm <634482545@qq.com>
2021-12-29 17:14:00 +0800

    生产不打印错误

--- public/index.php
+++ public/index.php
@@ -5,4 +5,11 @@ define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../'));
 
 $application = new \Yaf\Application(APPLICATION_PATH . "/conf/application.ini", 'product');
 
+# 线上环境不打印错误
+if (\Yaf\ENVIRON == 'product' && config('display_errors')) {
+    error_reporting(0);
+} else {
+    error_reporting(E_ALL & ~E_WARNING);
+}
+
 $application->bootstrap()->run();
